4d75021c8ebb694574ef7583eb6c2db0f3fbf230
Javier López de Ancos <javierlopezdeancos@gmail.com>
2024-04-19 12:47:16 +0200

    feat: add blog section

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,15 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-const markdownIt = require('markdown-it');
-const pluginRss = require('@11ty/eleventy-plugin-rss');
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const markdownIt = require("markdown-it");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
-module.exports = function(eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("src/styles");
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/styles");
 
-	eleventyConfig.addPlugin(pluginWebc, {
-		components: "src/components/*.webc",
-    layouts: "src/layouts/*.webc"
-	});
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: ["src/components/**/*.webc", "npm:@11ty/eleventy-plugin-syntaxhighlight/*.webc"],
+    layouts: "src/layouts/*.webc",
+  });
 
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(pluginRss);
@@ -17,16 +17,32 @@ module.exports = function(eleventyConfig) {
   const markdownParserOptions = {
     html: true,
     breaks: true,
-    linkify: false
+    linkify: false,
   };
 
-  eleventyConfig.setLibrary('md', markdownIt(markdownParserOptions));
+  eleventyConfig.setLibrary("md", markdownIt(markdownParserOptions));
 
-	return {
+  const english = new Intl.DateTimeFormat("en");
+
+  eleventyConfig.addFilter("niceDate", function (uglyDate) {
+    return english.format(uglyDate);
+  });
+
+  eleventyConfig.setServerOptions({
+		liveReload: true,
+		domDiff: true,
+		port: 8080,
+		watch: [],
+		showAllHosts: false,
+		encoding: "utf-8",
+		showVersion: false,
+	});
+
+  return {
     dir: {
       // ⚠️ These values are both relative to your input directory.
       includes: "_includes",
-      layouts: "src/layouts"
-    }
-  }
+      layouts: "src/layouts",
+    },
+  };
 };
