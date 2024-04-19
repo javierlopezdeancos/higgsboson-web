47a24305368d6d4d9a4810747e3f0693e05021fe
Javier López de Ancos <javier@atomicjar.com>
2024-04-19 10:59:45 +0000

    Add blog (#10)
    * chore: add plugins to rss and syntax hightlight
    
    * fix: add margin botton to main
    
    * chore: install markdown parser
    
    * chore: add config to use new different plugins
    
    * chore: add idea editor config files to ignore
    
    * chore: add description and keywords into the package
    
    * update components and adapt to better responsive
    
    * feat: add blog section
    
    * fix: build correct page set correct permalink
    
    * fix: set correct link to blog item in navigation bar

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,48 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const markdownIt = require("markdown-it");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
-module.exports = function(eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("src/styles");
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/styles");
 
-	eleventyConfig.addPlugin(pluginWebc, {
-		components: "src/components/*.webc",
-    layouts: "src/layouts/*.webc"
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: ["src/components/**/*.webc", "npm:@11ty/eleventy-plugin-syntaxhighlight/*.webc"],
+    layouts: "src/layouts/*.webc",
+  });
+
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(pluginRss);
+
+  const markdownParserOptions = {
+    html: true,
+    breaks: true,
+    linkify: false,
+  };
+
+  eleventyConfig.setLibrary("md", markdownIt(markdownParserOptions));
+
+  const english = new Intl.DateTimeFormat("en");
+
+  eleventyConfig.addFilter("niceDate", function (uglyDate) {
+    return english.format(uglyDate);
+  });
+
+  eleventyConfig.setServerOptions({
+		liveReload: true,
+		domDiff: true,
+		port: 8080,
+		watch: [],
+		showAllHosts: false,
+		encoding: "utf-8",
+		showVersion: false,
 	});
 
-	return {
+  return {
     dir: {
       // ⚠️ These values are both relative to your input directory.
       includes: "_includes",
-      layouts: "src/layouts"
-    }
-  }
+      layouts: "src/layouts",
+    },
+  };
 };
---
+++ posts/hello-world.md
@@ -0,0 +1,18 @@
+---
+title: Hello World
+date: 2024-04-01
+categories: ["javascript","cats"]
+---
+
+Hi 👋 there!, this is the first post that will be part of a series in which we will illustrate the entire process of creating higgsboson. Probably the first thing you notice is that my level of English is not native and that makes you uncomfortable, I am very sorry if this is the case but I thought it would be useful, even though it is not the language I speak, to write the posts in this way, you learn about the process, I learn to express myself in written English by practicing.
+
+```js
+// Comment
+export function SayHelloWorld (data) {
+  return `Hello World`;
+}
+
+SayHelloWorld();
+```
+
+Long life and prosperity 🖖, see you in the next post.
