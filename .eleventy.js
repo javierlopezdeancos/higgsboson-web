a64e2f40ad0b227fd2ee5f00b9a3898ecfeb88c7
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2024-05-31 11:54:49 +0200

    chore: add plugin to add classes by tag in markdown

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const markdownIt = require("markdown-it");
+const markdownItClass = require('@toycode/markdown-it-class')
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
@@ -20,7 +21,9 @@ module.exports = function (eleventyConfig) {
     linkify: false,
   };
 
-  eleventyConfig.setLibrary("md", markdownIt(markdownParserOptions));
+  const mapping = { p: 'hggs-text', a: 'hggs-link', h1: 'hggs-heading', h2: 'hggs-heading', h3: 'hggs-heading', h4: 'hggs-heading', h5: 'hggs-heading', h6: 'hggs-heading', ul: 'hggs-list', ol: 'hggs-list', li: 'hggs-list-item', blockquote: 'hggs-blockquote', code: 'hggs-code', pre: 'hggs-pre' }
+
+  eleventyConfig.setLibrary("md", markdownIt(markdownParserOptions).use(markdownItClass, mapping));
 
   const english = new Intl.DateTimeFormat("en");
 
