00126ca5d807e4c973a73210f232fa986be47274
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2024-06-06 14:36:30 +0200

    fix: fix problems with light theme

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,11 @@
-const pluginWebc = require("@11ty/eleventy-plugin-webc");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const webcPlugin = require("@11ty/eleventy-plugin-webc");
+const syntaxHighlightPlugin = require("@11ty/eleventy-plugin-syntaxhighlight");
 const markdownIt = require("markdown-it");
 const markdownItClass = require('@toycode/markdown-it-class')
-const pluginRss = require("@11ty/eleventy-plugin-rss");
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/styles");
-
-  eleventyConfig.addPlugin(pluginWebc, {
-    components: ["src/components/**/*.webc", "npm:@11ty/eleventy-plugin-syntaxhighlight/*.webc"],
-    layouts: "src/layouts/*.webc",
-  });
-
-  eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPlugin(pluginRss);
 
   const markdownParserOptions = {
     html: true,
@@ -25,10 +17,37 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.setLibrary("md", markdownIt(markdownParserOptions).use(markdownItClass, mapping));
 
-  const english = new Intl.DateTimeFormat("en");
-
   eleventyConfig.addFilter("niceDate", function (uglyDate) {
+    const english = new Intl.DateTimeFormat("en");
     return english.format(uglyDate);
+  });
+
+  // Get the first `n` elements of a collection.
+	eleventyConfig.addFilter("head", (array, n) => {
+		if(!Array.isArray(array) || array.length === 0) {
+			return [];
+		}
+		if( n < 0 ) {
+			return array.slice(n);
+		}
+
+		return array.slice(0, n);
+	});
+
+  // Return all the tags used in a collection
+	eleventyConfig.addFilter("getAllTags", collection => {
+		let tagSet = new Set();
+		for(let item of collection) {
+			(item.data.tags || []).forEach(tag => tagSet.add(tag));
+		}
+		return Array.from(tagSet);
+	});
+
+  eleventyConfig.addPlugin(syntaxHighlightPlugin);
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+  eleventyConfig.addPlugin(webcPlugin, {
+    components: ["src/components/**/*.webc", "npm:@11ty/eleventy-plugin-syntaxhighlight/*.webc"],
+    layouts: "src/layouts/*.webc",
   });
 
   eleventyConfig.setServerOptions({
